fb173bc9168d214186152349c43e749dc35c150e
Matt Youill <matt.youill@burnian.com>
2018-05-16 11:54:25 +1000

    Add more sorting tests.

--- tests/test_sort.py
+++ tests/test_sort.py
@@ -2,12 +2,13 @@
 """Sort query tests
 
 """
+
 from op.collate import Collate
 from op.sort import Sort
 from op.table_scan import TableScan
 
 
-def test_sort():
+def test_sort_asc():
     """Executes a sorted query. The results are collated.
 
     :return: None
@@ -35,6 +36,38 @@ def test_sort():
             prev = t
         else:
             assert float(t[5]) > float(prev[5])
+
+    assert num_rows == 10000
+
+
+def test_sort_desc():
+    """Executes a sorted query. The results are collated.
+
+    :return: None
+    """
+
+    num_rows = 0
+
+    # Query plan
+    ts = TableScan('supplier.csv', 'select * from S3Object;')
+    s = Sort(5, float, 'DESC')
+    c = Collate()
+
+    ts.connect(s)
+    s.connect(c)
+
+    # Start the query
+    ts.start()
+
+    # Assert the results
+    prev = None
+    for t in c.tuples:
+        num_rows += 1
+        # print("{}:{}".format(num_rows, t))
+        if prev is None:
+            prev = t
+        else:
+            assert float(t[5]) < float(prev[5])
 
     assert num_rows == 10000
 
