c780c7066a9cb02a13250c59ec54b5d219e163c4
Matt Youill <matt.youill@burnian.com>
2018-05-28 19:00:44 +1000

    Readd perf tests (somehow missing from commit)

---
+++ perf/join/run.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""Join query runner
+
+Runs the join query tests
+
+"""
+import join
+
+
+def main():
+    join.main()
+
+
+if __name__ == "__main__":
+    main()
---
+++ perf/topk/run.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""Limit top k query runner
+
+Runs the top k query tests
+
+"""
+
+from perf.topk import simple, limit
+
+
+def main():
+    limit.main()
+    simple.main()
+
+
+if __name__ == "__main__":
+    main()
