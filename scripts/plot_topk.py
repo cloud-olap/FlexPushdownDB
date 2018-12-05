7363a998b35c9f68bf6d7d1ee8a8b032ae626816
Xiangyao Yu <xiangyaoyu@Xiangyaos-MacBook-Air.local>
2018-12-04 22:15:31 -0500

    plot_topk

--- scripts/plot_topk.py
+++ scripts/plot_topk.py
@@ -19,25 +19,57 @@ def parse(fname):
         items = re.findall(r"total_elapsed_time: \d*\.?\d+", lines)
         total_time = float(items[0].split(':')[1])
         
+        items = re.findall(r"sql_scanned_bytes: \d*\.?\d+", lines)
+        sql_scanned_bytes = float(items[0].split(':')[1])
+
+        items = re.findall(r"sql_returned_bytes: \d*\.?\d+", lines)
+        sql_returned_bytes = float(items[0].split(':')[1])
+        
+        items = re.findall(r"[^_]returned_bytes: \d*\.?\d+", lines)
+        returned_bytes = float(items[0].split(':')[1])
+        
+        items = re.findall(r"num_http_get_requests: \d*\.?\d+", lines)
+        num_http_get_requests = float(items[0].split(':')[1])
+        
+        items = re.findall(r"bytes_returned': '\d*\.?\d+", lines)
+        total_bytes_returned = sum( [ float(x.split(": '")[1]) for x in items ] )
+        
+        items = re.findall(r"bytes_scanned': \d*\.?\d+", lines)
+        total_bytes_scanned = sum( [ float(x.split(':')[1]) for x in items ] )
+
+        """
+        items = re.findall(r"total_elapsed_time: \d*\.?\d+", lines)
+        total_time = float(items[0].split(':')[1])
+        
         items = re.findall(r"sql_returned_bytes: \d+", lines)
         total_bytes_returned = sum( [ float(x.split(":")[1]) for x in items ] )
         
         items = re.findall(r"sql_scanned_bytes: \d+", lines)
         total_bytes_scanned = sum( [ float(x.split(':')[1]) for x in items ] )
+        """
         try:
             items = re.findall(r"sql_gen: \d*\.?\d+", lines)
             phase1_time = float(items[0].split(':')[1])
             phase2_time = total_time - phase1_time
         except:
             pass
-
-    return total_time, total_bytes_returned, total_bytes_scanned, phase1_time, phase2_time
-
+    return total_time, total_bytes_returned, total_bytes_scanned, \
+           sql_scanned_bytes, sql_returned_bytes, returned_bytes, num_http_get_requests, \
+           phase1_time, phase2_time
+    #return total_time, total_bytes_returned, total_bytes_scanned, phase1_time, phase2_time
 def calculate_cost(res):
-    data_cost = res[2] / 1024.0**3 * 0.002 + res[1] / 1024**3 * 0.0007 
+    transfer_cost = res[4] / 1024**3 * 0.0007
+    scan_cost = res[3] / 1024.0**3 * 0.002
+    request_cost = res[6] * 0.0000004    
     comp_cost = res[0] / 3600.0 * 2.128 
-    total_cost = data_cost + comp_cost
-    return total_cost, data_cost, comp_cost
+    total_cost = transfer_cost + scan_cost + request_cost + comp_cost
+    return total_cost, transfer_cost, scan_cost, request_cost, comp_cost
+
+#def calculate_cost(res):
+#    data_cost = res[2] / 1024.0**3 * 0.002 + res[1] / 1024**3 * 0.0007 
+#    comp_cost = res[0] / 3600.0 * 2.128 
+#    total_cost = data_cost + comp_cost
+#    return total_cost, data_cost, comp_cost
 
 ##############################
 ## Sample Size: runtime and data returned 
@@ -64,8 +96,8 @@ for sample_size in sample_sizes:
         #print items
         rts.append(items[0])
         returned.append(items[1])
-        ph1.append(items[3])
-        ph2.append(items[4])
+        ph1.append(items[7])
+        ph2.append(items[8])
     idx = rts.index(min(rts))
     data.append(rts[idx]) 
     traffic.append(returned[idx] / 1024**3)
@@ -101,30 +133,53 @@ k = 100
 sample_sizes = [ 10**3, 10**4, 10**5, 10**6, 10**7]
 
 trials = [1, 2, 3]
-data_cost = []
-compute_cost = []
 width=0.6
+transfer_cost = []
+scan_cost = []
+request_cost = []
+comp_cost = []
 for sample_size in sample_sizes:
-    dc = []
-    cc = []
     rts = []
+    vals = []
     for trial in trials:
         #return total_time, total_bytes_returned, total_bytes_scanned, phase1_time, phase2_time
         items = parse('aws-exps/topk/sample_k100_sample{}_trial{}.txt'.format(sample_size, trial))
-        costs = calculate_cost(items)
-        dc.append(costs[1])
-        cc.append(costs[2])
+        val = calculate_cost(items)
+        vals.append(val)
         rts.append(items[0])
-    idx = rts.index(min(rts))
-    data_cost.append(dc[idx])
-    compute_cost.append(cc[idx])
+    min_index = rts.index( min(rts) )
+    transfer_cost.append(   vals[min_index][1])
+    scan_cost.append(       vals[min_index][2])
+    request_cost.append(    vals[min_index][3])
+    comp_cost.append(       vals[min_index][4])
+print (transfer_cost)
 
-ax.bar(range(len(sample_sizes)), data_cost, width=width, color=colors[0], hatch='//')
-ax.bar(range(len(sample_sizes)), compute_cost, bottom=data_cost, width=width, color=colors[1])
+bottom = [0] * len(comp_cost)
+# plot legend
+b_compute = ax.bar([x for x in range(5)], bottom, width=width, color=colors[3], label='Compute Cost')
+b_request = ax.bar([x for x in range(5)], bottom, width=width, color=colors[2], hatch='xxx', label='Request Cost')
+b_scan = ax.bar([x    for x in range(5)], bottom, width=width, color=colors[1], hatch='\\\\\\', label='Scan Cost')
+b_transfer = ax.bar([x for x in range(5)], bottom, width=width, color=colors[0], hatch='////', label='Transfer Cost')
+
+# plot bars 
+ax.bar([x for x in range(5)], transfer_cost, width=width, color=colors[0], hatch='////')
+bottom = [x + y for x, y in zip(bottom, transfer_cost)]
+ax.bar([x for x in range(5)], scan_cost, bottom=bottom, width=width, color=colors[1], hatch='\\\\\\')
+bottom = [x + y for x, y in zip(bottom, scan_cost)]
+ax.bar([x for x in range(5)], request_cost, bottom=bottom, width=width, color=colors[2], hatch='xxx')
+bottom = [x + y for x, y in zip(bottom, request_cost)]
+ax.bar([x for x in range(5)], comp_cost, bottom=bottom, width=width, color=colors[3])
+
+#ax.bar(range(len(sample_sizes)), data_cost, width=width, color=colors[0], hatch='//')
+#ax.bar(range(len(sample_sizes)), compute_cost, bottom=data_cost, width=width, color=colors[1])
 
 ax.set_xticks([x for x in range(len(sample_sizes))])
 ax.set_xlim([-0.6, 4.6])
 plt.subplots_adjust(left=0.14, right=0.98, bottom=0.15, top=0.92)
+
+ax.legend([b_compute[0], b_request[0], b_scan[0], b_transfer[0]], 
+    ['Compute Cost', 'Request Cost', 'Scan Cost', 'Transfer Cost'], 
+    ncol=1, bbox_to_anchor=[0.98, 0.6], fontsize=14)
 
 ax.set_xticklabels(['$10^3$', '$10^4$', '$10^5$', '$10^6$', '$10^7$'])
 ax.set_xlabel('Sample Size')
@@ -181,32 +236,59 @@ ks = [1, 10, 100, 1000, 10000, 100000]
 
 trials = [1, 2, 3]
 width=0.3
-for n, config in enumerate(['baseline', 'sample']):
-    data_cost = []
-    compute_cost = []
-    for k in ks: 
+bars = [0] * 2
+labels = ['Server-Side Top-K', 'Sampled Top-K']
+for cid, config in enumerate(['baseline', 'sample']):
+    transfer_cost = []
+    scan_cost = []
+    request_cost = []
+    comp_cost = []
+    for k in ks:
         rts = []
-        costs = []
-        dc = []
-        cc = []
+        vals = []
+        #dc = []
+        #cc = []
         for trial in trials:
             items = parse('aws-exps/topk/{}_k{}_trial{}.txt'.format(config, k, trial))
-            costs = calculate_cost(items)
-            dc.append(costs[1])
-            cc.append(costs[2])
+            c = calculate_cost(items)
             rts.append(items[0])
-        idx = rts.index(min(rts))
-        data_cost.append(dc[idx])
-        compute_cost.append(cc[idx])
-    label = 'Server-Side Top-K' if n == 0 else 'Sampled Top-K'
-    ax.bar([x + n*width for x in range(len(ks))], data_cost, width=width, color=colors[n], hatch='//')
-    ax.bar([x + n*width for x in range(len(ks))], compute_cost, label=label, bottom=data_cost, width=width, color=colors[n])
+            vals.append(c)
+        min_index = rts.index(min(rts))
+        transfer_cost.append(   vals[min_index][1])
+        scan_cost.append(       vals[min_index][2])
+        request_cost.append(    vals[min_index][3])
+        comp_cost.append(       vals[min_index][4])
+
+    bottom = [0] * len(comp_cost)
+    # plot legend
+    
+    b_compute = ax.bar([x + cid * width for x in range(6)], bottom, width=width, color='w', label='Compute Cost')
+    b_request = ax.bar([x + cid * width for x in range(6)], bottom, width=width, color='w', hatch='xxx', label='Request Cost')
+    b_scan = ax.bar([x + cid * width for x in range(6)], bottom, width=width, color='w', hatch='\\\\\\', label='Scan Cost')
+    b_transfer = ax.bar([x + cid * width for x in range(6)], bottom, width=width, color='w', hatch='////', label='Transfer Cost')
+
+    # plot bars 
+    ax.bar([x + cid * width for x in range(6)], transfer_cost, width=width, color=colors[cid], hatch='////')
+    bottom = [x + y for x, y in zip(bottom, transfer_cost)]
+    ax.bar([x + cid * width for x in range(6)], scan_cost, bottom=bottom, width=width, color=colors[cid], hatch='\\\\\\')
+    bottom = [x + y for x, y in zip(bottom, scan_cost)]
+    ax.bar([x + cid * width for x in range(6)], request_cost, bottom=bottom, width=width, color=colors[cid], hatch='xxx')
+    bottom = [x + y for x, y in zip(bottom, request_cost)]
+    b = ax.bar([x + cid * width for x in range(6)], comp_cost, bottom=bottom, label=labels[cid], width=width, color=colors[cid])
+    bars[cid] = b[0]
+
 
 ax.set_xticks([x + width/2 for x in range(len(ks))])
 ax.set_xlim([-0.45, 5.75])
-ax.legend(ncol=2, loc='best', fontsize=14)
-plt.subplots_adjust(left=0.12, right=0.98, bottom=0.15, top=0.92)
 
+lg1 = ax.legend(bars, labels, ncol=3, bbox_to_anchor=[0.99, 1.18], fontsize=14)
+lg2 = ax.legend([b_compute[0], b_request[0], b_scan[0], b_transfer[0]], ['Compute Cost', 'Request Cost', 'Scan Cost', 'Transfer Cost'], 
+        ncol=1, bbox_to_anchor=[0.35, 1.02], fontsize=14)
+plt.gca().add_artist(lg1)
+plt.gca().add_artist(lg2)
+
+#ax.legend(ncol=2, loc='best', fontsize=14)
+plt.subplots_adjust(left=0.12, right=0.98, bottom=0.15, top=0.88)
 #ax.set_xticklabels(['$10^2$', '$10^3$', '$10^4$', '$10^5$', '$10^6$', '$10^7$'])
 ax.set_xticklabels(['$1$', '$10$', '$10^2$', '$10^3$', '$10^4$', '$10^5$'])
 ax.set_xlabel('K')
