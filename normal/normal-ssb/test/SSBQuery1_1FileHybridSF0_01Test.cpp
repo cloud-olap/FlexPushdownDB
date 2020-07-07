7b9d9dadc279b7fb2239f9d58e42c3790bd387a1
Matt Youill <matt.youill@burnian.com>
2020-07-07 12:53:23 +1000

    Hyrbid Test shim

---
+++ normal/normal-ssb/test/SSBQuery1_1FileHybridSF0_01Test.cpp
@@ -0,0 +1,19 @@
+//
+// Created by matt on 24/4/20.
+//
+
+#include <doctest/doctest.h>
+
+#include <normal/ssb/query1_1/LocalFileSystemTests.h>
+
+using namespace normal::ssb;
+
+#define SKIP_SUITE false
+
+TEST_SUITE ("ssb-query1.1-file-hybrid-sf0.01" * doctest::skip(SKIP_SUITE)) {
+
+TEST_CASE ("date-scan-par1" * doctest::skip(false || SKIP_SUITE)) {
+  LocalFileSystemTests::dateScan("data/ssb-sf0.01", 1, true);
+}
+
+}
