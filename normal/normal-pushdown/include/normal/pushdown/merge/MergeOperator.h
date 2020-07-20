eda82f9c338c9536fa13286bae3fd4adecafce8c
Matt Youill <matt.youill@burnian.com>
2020-07-20 15:14:36 +1000

    Add merge operator

---
+++ normal/normal-pushdown/include/normal/pushdown/merge/MergeOperator.h
@@ -0,0 +1,42 @@
+//
+// Created by matt on 20/7/20.
+//
+
+#ifndef NORMAL_NORMAL_PUSHDOWN_INCLUDE_NORMAL_PUSHDOWN_MERGE_MERGEOPERATOR_H
+#define NORMAL_NORMAL_PUSHDOWN_INCLUDE_NORMAL_PUSHDOWN_MERGE_MERGEOPERATOR_H
+
+#include <normal/core/Operator.h>
+#include <normal/core/message/CompleteMessage.h>
+#include <normal/pushdown/TupleMessage.h>
+#include <queue>
+
+using namespace normal::core;
+using namespace normal::core::message;
+
+namespace normal::pushdown::merge {
+
+class MergeOperator : public Operator {
+
+public:
+
+  explicit MergeOperator(const std::string &Name);
+
+  void onReceive(const Envelope &msg) override;
+
+  void onStart();
+  void onComplete(const CompleteMessage &);
+  void onTuple(const TupleMessage &message);
+
+private:
+
+  std::optional<LocalOperatorDirectoryEntry> leftProducer_  = std::nullopt;
+  std::optional<LocalOperatorDirectoryEntry> rightProducer_  = std::nullopt;
+
+  std::list<std::shared_ptr<TupleSet2>> leftTupleSets_;
+  std::list<std::shared_ptr<TupleSet2>> rightTupleSets_;
+
+};
+
+}
+
+#endif //NORMAL_NORMAL_PUSHDOWN_INCLUDE_NORMAL_PUSHDOWN_MERGE_MERGEOPERATOR_H
---
+++ normal/normal-pushdown/src/merge/MergeOperator.cpp
@@ -0,0 +1,92 @@
+//
+// Created by matt on 20/7/20.
+//
+
+#include <normal/tuple/TupleSet2.h>
+#include "normal/pushdown/merge/MergeOperator.h"
+#include "normal/pushdown/merge/MergeKernel.h"
+
+using namespace normal::pushdown::merge;
+
+MergeOperator::MergeOperator(const std::string &Name) :
+	Operator(Name, "Merge") {
+}
+
+void MergeOperator::onReceive(const Envelope &msg) {
+  if (msg.message().type() == "StartMessage") {
+	this->onStart();
+  } else if (msg.message().type() == "TupleMessage") {
+	auto tupleMessage = dynamic_cast<const TupleMessage &>(msg.message());
+	this->onTuple(tupleMessage);
+  } else if (msg.message().type() == "CompleteMessage") {
+	auto completeMessage = dynamic_cast<const CompleteMessage &>(msg.message());
+	this->onComplete(completeMessage);
+  } else {
+	// FIXME: Propagate error properly
+	throw std::runtime_error("Unrecognized message type " + msg.message().type());
+  }
+}
+
+void MergeOperator::onStart() {
+
+  auto producers_ = this->ctx()->operatorMap().get(OperatorRelationshipType::Producer);
+  leftProducer_ = producers_[0];
+  rightProducer_ = producers_[1];
+
+  SPDLOG_DEBUG("Starting '{}'  |  leftProducer: {}, rightProducer: {}",
+			   name(),
+			   leftProducer_->name(),
+			   rightProducer_->name());
+}
+
+void MergeOperator::onComplete(const CompleteMessage &) {
+  if (ctx()->operatorMap().allComplete(OperatorRelationshipType::Producer)) {
+	ctx()->notifyComplete();
+  }
+}
+
+void MergeOperator::onTuple(const TupleMessage &message) {
+
+  // Get the tuple set
+  const auto &tupleSet = TupleSet2::create(message.tuples());
+  if (tupleSet->numRows() == 0) {
+	return;
+  }
+
+  // Add the tupleset to a slot in left or right producers tuple queue
+  if (message.sender() == leftProducer_->name()) {
+	leftTupleSets_.emplace_back(tupleSet);
+  } else if (message.sender() == leftProducer_->name()) {
+	rightTupleSets_.emplace_back(tupleSet);
+  } else {
+	throw std::runtime_error(fmt::format("Unrecognized producer {}", message.sender()));
+  }
+
+  // Check if we have merge-able tuple sets
+  while (!leftTupleSets_.empty() && !rightTupleSets_.empty()) {
+
+    // Take next left and right tuplesets from the queues
+	auto leftTupleSet = leftTupleSets_.front();
+	auto rightTupleSet = rightTupleSets_.front();
+
+	// Merge tuplesets
+	auto expectedMergedTupleSet = MergeKernel::merge(leftTupleSet, rightTupleSet);
+
+	if (!expectedMergedTupleSet.has_value()) {
+	  throw std::runtime_error(expectedMergedTupleSet.error());
+	} else {
+	  // Send merged tupleset
+	  auto mergedTupleSet = expectedMergedTupleSet.value();
+	  std::shared_ptr<core::message::Message>
+		  tupleMessage = std::make_shared<core::message::TupleMessage>(mergedTupleSet->toTupleSetV1(), name());
+	  ctx()->tell(tupleMessage);
+	}
+
+	// Pop the processed tuple sets from the queues
+	leftTupleSets_.pop_front();
+	rightTupleSets_.pop_front();
+  }
+
+}
+
+
