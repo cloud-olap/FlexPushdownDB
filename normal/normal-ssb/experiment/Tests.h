d17ab934738587dc8e4f810d2654fa3065b0619f
Yifei Yang <yiy032@eng.ucsd.edu>
2020-10-10 13:57:10 -0700

    Enable parameters on test executable

---
+++ normal/normal-ssb/experiment/Tests.h
@@ -0,0 +1,14 @@
+//
+// Created by Yifei Yang on 10/10/20.
+//
+
+#ifndef NORMAL_TESTS_H
+#define NORMAL_TESTS_H
+
+#include <cstddef>
+
+namespace normal::ssb {
+  void mainTest(size_t cacheSize, int modeType, int cachingPolicyType);
+}
+
+#endif //NORMAL_TESTS_H
