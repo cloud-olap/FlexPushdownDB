9f67a24a09131c0dc8ebf2c810c4e731dd54bb17
Yifei Yang <yiy032@eng.ucsd.edu>
2020-10-10 13:57:01 -0700

    Enable parameters on test executable

--- normal/normal-ssb/experiment/ExperimentMain.cpp
+++ normal/normal-ssb/experiment/ExperimentMain.cpp
@@ -5,22 +5,35 @@
 #define DOCTEST_CONFIG_IMPLEMENT
 #include <doctest/doctest.h>
 #include "normal/ssb/Globals.h"
+#include "Tests.h"
+
+using namespace normal::ssb;
 
 const char* getCurrentTestName() { return doctest::detail::g_cs->currentTest->m_name; }
 const char* getCurrentTestSuiteName() { return doctest::detail::g_cs->currentTest->m_test_suite; }
 
 int main(int argc, char **argv) {
 
-  spdlog::set_level(spdlog::level::debug);
-  spdlog::set_pattern("[%H:%M:%S.%e] [thread %t] [%! (%s:%#)] [%l]  %v");
+//  spdlog::set_level(spdlog::level::debug);
+//  spdlog::set_pattern("[%H:%M:%S.%e] [thread %t] [%! (%s:%#)] [%l]  %v");
+//
+//  doctest::Context context;
+//
+//  context.applyCommandLine(argc, argv);
+//  int rc = context.run();
+//
+//  if (context.shouldExit()) // important - query flags (and --exit) rely on the user doing this
+//    return rc;
+//
+//  return rc;
 
-  doctest::Context context;
+  auto cacheSize = (size_t) (atof(argv[1]) * 1024*1024*1024);
+  auto modeType = atoi(argv[2]);
+  auto cachingPolicyType = atoi(argv[3]);
+  SPDLOG_INFO("Cache size: {}", cacheSize);
+  SPDLOG_INFO("Mode type: {}", modeType);
+  SPDLOG_INFO("CachingPolicy type: {}", cachingPolicyType);
+  mainTest(cacheSize, modeType, cachingPolicyType);
 
-  context.applyCommandLine(argc, argv);
-  int rc = context.run();
-
-  if (context.shouldExit()) // important - query flags (and --exit) rely on the user doing this
-    return rc;
-
-  return rc;
+  return 0;
 }
