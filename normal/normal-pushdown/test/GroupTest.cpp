986510dbfcdd77a806904746aab11f440af36ee8
Matt Youill <matt.youill@burnian.com>
2020-10-20 12:30:37 +1100

    Split grouping into a "kernel" for simpler testing and benchmarking clean up group a bit

--- normal/normal-pushdown/test/GroupTest.cpp
+++ normal/normal-pushdown/test/GroupTest.cpp
@@ -3,93 +3,87 @@
 //
 
 #include <memory>
+#include <filesystem>
 
 #include <doctest/doctest.h>
 
-#include <normal/pushdown/Collate.h>
-#include <normal/core/OperatorManager.h>
-#include <normal/core/graph/OperatorGraph.h>
+#include <normal/core/Normal.h>
 #include <normal/pushdown/file/FileScan.h>
 #include <normal/pushdown/group/Group.h>
-#include <normal/tuple/TupleSet2.h>
 #include <normal/pushdown/aggregate/Sum.h>
 #include <normal/expression/gandiva/Column.h>
 #include <normal/core/type/Float64Type.h>
 #include <normal/expression/gandiva/Cast.h>
 #include "TestUtil.h"
 
+using namespace std::filesystem;
 using namespace normal::pushdown;
 using namespace normal::pushdown::test;
 using namespace normal::pushdown::group;
-using namespace normal::tuple;
 using namespace normal::core::type;
-using namespace normal::core::graph;
-using namespace normal::expression;
 using namespace normal::expression::gandiva;
 
-#define SKIP_SUITE true
+TEST_SUITE ("group") {
 
-TEST_SUITE ("group" * doctest::skip(SKIP_SUITE)) {
+TEST_CASE ("group" * doctest::skip(false)) {
+  {
+	auto n = Normal::start();
+	auto g = n->createQuery();
 
-TEST_CASE ("group" * doctest::skip(false || SKIP_SUITE)) {
+	auto aFile = filesystem::absolute("data/group/a.csv");
+	auto numBytesAFile = filesystem::file_size(aFile);
 
-  auto aFile = filesystem::absolute("data/group/a.csv");
-  auto numBytesAFile = filesystem::file_size(aFile);
+	auto scan = FileScan::make("fileScan",
+							   "data/group/a.csv",
+							   std::vector<std::string>{"AA", "AB"},
+							   0,
+							   numBytesAFile,
+							   g->getId(),
+							   true);
+	g->put(scan);
+	auto sumExpr = std::make_shared<normal::pushdown::aggregate::Sum>("sum",
+																	  cast(col("AB"), float64Type()));
+	auto
+		expressions2 =
+		std::make_shared<std::vector<std::shared_ptr<normal::pushdown::aggregate::AggregationFunction>>>();
+	expressions2->push_back(sumExpr);
 
-  auto mgr = std::make_shared<normal::core::OperatorManager>();
+	auto group = Group::make("group", {"AA"}, expressions2);
+	g->put(group);
+	auto collate = std::make_shared<Collate>("collate", g->getId());
+	g->put(collate);
 
-  auto g = OperatorGraph::make(mgr);
+	scan->produce(group);
+	group->consume(scan);
 
-  auto scan = FileScan::make("fileScan", "data/group/a.csv", std::vector<std::string>{"AA", "AB"}, 0, numBytesAFile, g->getId());
-  auto sumExpr = std::make_shared<normal::pushdown::aggregate::Sum>("sum",
-																	cast(col("AB"), float64Type()));
-  auto
-	  expressions2 = std::make_shared<std::vector<std::shared_ptr<normal::pushdown::aggregate::AggregationFunction>>>();
-  expressions2->push_back(sumExpr);
+	group->produce(collate);
+	collate->consume(group);
 
-  auto group = Group::make("group", {"AA"}, expressions2);
-  auto collate = std::make_shared<Collate>("collate", g->getId());
+	TestUtil::writeExecutionPlan(*g);
 
-  scan->produce(group);
-  group->consume(scan);
+	auto expectedTupleSet = g->execute();
+		REQUIRE(expectedTupleSet.has_value());
+	auto tupleSet = expectedTupleSet.value();
 
-  group->produce(collate);
-  collate->consume(group);
+	SPDLOG_INFO("Output:\n{}", tupleSet->showString(TupleSetShowOptions(TupleSetShowOrientation::RowOriented)));
 
-  g->put(scan);
-  g->put(group);
-  g->put(collate);
+		REQUIRE_EQ(tupleSet->numRows(), 2);
+		REQUIRE_EQ(tupleSet->numColumns(), 2);
 
-  TestUtil::writeExecutionPlan(*g);
+	/*
+	 * FIXME: The following assumes the output is produced in a specific order but this shouldn't necessarily
+	 *  be assumed. Will only be able to check properly once we have a sort operator
+	 */
+	auto columnAA = tupleSet->getColumnByName("AA").value();
+		REQUIRE_EQ(columnAA->element(0).value()->value<std::string>(), "10");
+		REQUIRE_EQ(columnAA->element(1).value()->value<std::string>(), "12");
 
-  mgr->boot();
-
-  mgr->start();
-//  mgr->join();
-//
-//  auto tuples = collate->tuples();
-
-  mgr->stop();
-
-//  auto tupleSet = TupleSet2::create(tuples);
-//
-//  SPDLOG_INFO("Output:\n{}", tupleSet->showString(TupleSetShowOptions(TupleSetShowOrientation::RowOriented)));
-//
-//	  CHECK(tupleSet->numRows() == 2);
-//	  CHECK(tupleSet->numColumns() == 2);
-//
-//  /*
-//   * FIXME: The following assumes the output is produced in a specific order but this shouldn't necessarily
-//   *  be assumed. Will only be able to check properly once we have a sort operator
-//   */
-//  auto columnAA = tupleSet->getColumnByName("AA").value();
-//	  CHECK(columnAA->element(0).value()->value<long>() == 10);
-//	  CHECK(columnAA->element(1).value()->value<long>() == 12);
-//
-//  auto columnAB = tupleSet->getColumnByName("sum").value();
-//	  CHECK(columnAB->element(0).value()->value<double>() == 27);
-//	  CHECK(columnAB->element(1).value()->value<double>() == 15);
-
+	auto columnAB = tupleSet->getColumnByName("sum").value();
+		REQUIRE_EQ(columnAB->element(0).value()->value<double>(), 27);
+		REQUIRE_EQ(columnAB->element(1).value()->value<double>(), 15);
+	n->stop();
+  }
+	  REQUIRE_EQ(::arrow::default_memory_pool()->bytes_allocated(), 0);
 }
 
 }
