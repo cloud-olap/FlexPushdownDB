a23b92b2b7819e0b3b2fbc2495e453812b311b6c
Yifei Yang <yiy032@eng.ucsd.edu>
2021-03-11 23:50:40 -0800

    For filter, make compilation of ::gandiva::Filter and ::gandiva::Projector only occurs once (not fix local bottleneck)

--- normal/normal-expression-gandiva/include/normal/expression/gandiva/Filter.h
+++ normal/normal-expression-gandiva/include/normal/expression/gandiva/Filter.h
@@ -10,6 +10,7 @@
 #include <gandiva/filter.h>
 #include <normal/expression/Filter.h>
 #include "Projector.h"
+#include <map>
 
 namespace normal::expression::gandiva {
 
@@ -21,15 +22,35 @@ public:
   static std::shared_ptr<Filter> make(const std::shared_ptr<Expression> &Pred);
 
   std::shared_ptr<normal::tuple::TupleSet2> evaluate(const normal::tuple::TupleSet2 &TupleSet) override;
-  void compile(const std::shared_ptr<normal::tuple::Schema> &Schema) override;
+  void compile(const std::shared_ptr<normal::tuple::Schema> &schema) override;
 
 private:
   std::shared_ptr<Expression> pred_;
-  ::gandiva::ExpressionPtr gandivaExpression_;
-  ::gandiva::ConditionPtr gandivaCondition_;
-  std::shared_ptr<::gandiva::Projector> gandivaProjector_;
   std::shared_ptr<::gandiva::Filter> gandivaFilter_;
+  std::shared_ptr<::gandiva::Projector> gandivaProjector_;
 };
+
+/**
+ * A global map to store compiled ::gandiva::Project and ::gandiva::Filter
+ */
+inline std::map<std::string, std::shared_ptr<::gandiva::Filter>> gandivaFilterMap_;
+inline std::map<std::string, std::shared_ptr<::gandiva::Projector>> gandivaProjectorMap_;
+
+/**
+ * Build and store ::gandiva::Filter
+ */
+std::shared_ptr<::gandiva::Filter> buildGandivaFilter(const std::shared_ptr<::gandiva::Condition> &gandivaCondition,
+                                                      const std::shared_ptr<normal::tuple::Schema> &schema);
+
+/**
+ * Build and store ::gandiva::Projector
+ */
+std::shared_ptr<::gandiva::Projector> buildGandivaProjector(const std::shared_ptr<normal::tuple::Schema> &schema);
+
+/**
+ * Clear ::gandiva::Projector and ::gandiva::Filter maps
+ */
+void clearGandivaProjectorAndFilter();
 
 }
 
