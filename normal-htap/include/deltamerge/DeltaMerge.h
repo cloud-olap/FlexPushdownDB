88158601570bc04c35b38ecf1747c7e98a3dc296
r0b3rt24 <hcao29@wisc.edu>
2021-08-25 18:27:58 -0400

    - Solved compiling errors for DeltaMerge.cpp

--- normal-htap/include/deltamerge/DeltaMerge.h
+++ normal-htap/include/deltamerge/DeltaMerge.h
@@ -8,32 +8,39 @@
 #include <normal/core/Operator.h>
 
 #include <normal/core/message/CompleteMessage.h>
-#include <normal/pushdown/TupleMessage.h>
+#include <normal/core/message/TupleMessage.h>
+#include <normal/tuple/TupleSet2.h>
 
 namespace normal::pushdown::deltamerge {
-    class DeltaMerge : public Operator {
+    class DeltaMerge : public core::Operator {
     public:
         explicit DeltaMerge(const std::string &Name, long queryId);
 
         static std::shared_ptr <DeltaMerge> make(const std::string &Name, long queryId);
 
-        void onReceive(const Envelope &msg) override;
+        void onReceive(const core::message::Envelope &msg) override;
 
         void onStart();
 
-        void onComplete(const CompleteMessage &);
+        void onComplete(const core::message::CompleteMessage &);
 
-        void onTuple(const TupleMessage &message);
+        void onTuple(const core::message::TupleMessage &message);
 
 
     private:
-        void DeltaMerge();
-
-        std::weak_ptr <std::unordered_map<std::string>, Operator> deltaProducers_;
-        std::weak_ptr <std::unordered_map<std::string>, Operator> stableProducers_;
+        std::unordered_map<std::string, std::weak_ptr<Operator>> deltaProducers_;
+        std::unordered_map<std::string, std::weak_ptr<Operator>> stableProducers_;
 
         std::list <std::shared_ptr<TupleSet2>> deltas_;
         std::list <std::shared_ptr<TupleSet2>> stables_;
+
+        void deltaMerge();
+
+        void addStableProducer(const std::shared_ptr<Operator> &stableProducer);
+
+        void addDeltaProducer(const std::shared_ptr<Operator> &deltaProducer);
+
+        bool allProducersComplete();
     }
 
 }
--- normal-htap/src/deltamerge/DeltaMerge.cpp
+++ normal-htap/src/deltamerge/DeltaMerge.cpp
@@ -2,7 +2,7 @@
 // Created by Han Cao on 7/15/21.
 //
 
-#include "normal/pushdown/deltamerge/DeltaMerge.h"
+#include <deltamerge/DeltaMerge.h>
 
 using namespace normal::pushdown::deltamerge;
 
@@ -14,14 +14,14 @@ std::shared_ptr <DeltaMerge> DeltaMerge::make(const std::string &Name, long quer
     return std::make_shared<DeltaMerge>(Name, queryId);
 }
 
-void DeltaMerge::onReceive(const Envelope &msg) {
+void DeltaMerge::onReceive(const core::message::Envelope &msg) {
     if (msg.message().type() == "StartMessage") {
         this->onStart();
     } else if (msg.message().type() == "TupleMessage") {
-        auto tupleMessage = dynamic_cast<const TupleMessage &>(msg.message());
+        auto tupleMessage = dynamic_cast<const core::message::TupleMessage &>(msg.message());
         this->onTuple(tupleMessage);
     } else if (msg.message().type() == "CompleteMessage") {
-        auto completeMessage = dynamic_cast<const CompleteMessage &>(msg.message());
+        auto completeMessage = dynamic_cast<const core::message::CompleteMessage &>(msg.message());
         this->onComplete(completeMessage);
     } else {
         // FIXME: Propagate error properly
@@ -34,21 +34,21 @@ void DeltaMerge::onStart() {
 }
 
 bool DeltaMerge::allProducersComplete() {
-    return ctx()->operatorMap().allComplete(OperatorRelationshipType::Producer);
+    return ctx()->operatorMap().allComplete(core::OperatorRelationshipType::Producer);
 }
 
-void DeltaMerge::onTuple(const TupleMessage &message) {
+void DeltaMerge::onTuple(const core::message::TupleMessage &message) {
     const auto &tupleSet = TupleSet2::create(message.tuples());
 
     if (deltaProducers_.find(message.sender()) != deltaProducers_.end()) {
         // means the sender is one of the delta producers
         deltas_.emplace_back(tupleSet);
     } else if (stableProducers_.find(message.sender()) != stableProducers_.end()) {
-        stables_emplace_back(tupleSet);
+        stables_.emplace_back(tupleSet);
     } else {
-        throw std::runtime_error(fmt::format("Unrecognized producer {}, left: {}, right: {}",
-                                             message.sender(), leftProducer_.lock()->name(),
-                                             rightProducer_.lock()->name()));
+//        throw std::runtime_error(fmt::format("Unrecognized producer {}, left: {}, right: {}",
+//                                             message.sender(), leftProducer_.lock()->name(),
+//                                             rightProducer_.lock()->name()));
     }
 
     // check if all producers complete
@@ -57,24 +57,24 @@ void DeltaMerge::onTuple(const TupleMessage &message) {
     }
 }
 
-void DeltaMerge::onComplete(const completeMessage &) {
+void DeltaMerge::onComplete(const core::message::CompleteMessage &) {
     // TODO: check with Yifei
-    if (!ctx()->isComplete() && ctx()->operatorMap().allComplete(OperatorRelationshipType::Producer)) {
+    if (!ctx()->isComplete() && ctx()->operatorMap().allComplete(core::OperatorRelationshipType::Producer)) {
         ctx()->notifyComplete();
     }
 }
 
 void DeltaMerge::addStableProducer(const std::shared_ptr <Operator> &stableProducer) {
-    stableProducers_[stableProducer.lock()->name()] = stableProducer;
+    stableProducers_[stableProducer->name()] = stableProducer;
     consume(stableProducer);
 }
 
 void DeltaMerge::addDeltaProducer(const std::shared_ptr <Operator> &deltaProducer) {
-    deltaProducers_[deltaProducer.lock()->name()] = deltaProducer;
+    deltaProducers_[deltaProducer->name()] = deltaProducer;
     consume(deltaProducer);
 }
 
-void DeltaMerge::DeltaMerge() {
+void DeltaMerge::deltaMerge() {
     // right now assume all the tuples that we have are in arrow format
 
     return;
